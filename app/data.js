a6b3ccdf96fd28636459ae222117e51710685d95
laszarm <hcrgames@gmail.com>
2019-07-14 22:31:00 +0200

    minor fixes added

--- app/data.js
+++ app/data.js
@@ -1,44 +1,57 @@
-const apiUrl = "http://localhost:3000";
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
 
-function createDom(data) {
-  let template = '<div class="container">';
+    const apiUrl = "http://localhost:3000";
 
-  for (const [index, company] of data.entries()) {
-    template += `<div class="d-flex flex-column">
-                  <button type="button" class="p-2 list-group-item list-group-item-action text-center" data-toggle="collapse" href="#collapse${index}" role="button" aria-expanded="false" aria-controls="collapse${index}">
+    function createDom(data) {
+      let template = '<div class="container">';
+
+      for (const [index, company] of data.entries()) {
+        template += `<div class="d-flex flex-column">
+                <button type="button" class="p-2 list-group-item list-group-item-action text-center" data-toggle="collapse" href="#collapse${index}" role="button" aria-expanded="false" aria-controls="collapse${index}">
                     ${company.name} <span class="badge badge-primary badge-pill">${company.users.length}</span>
-                  </button>`;
+                </button>`;
 
-    template += `<div class="collapse" id="collapse${index}">`;
+        template += `<div class="collapse" id="collapse${index}">`;
 
-    for (const user of company.users) {
-      if (user.uris.company === company.uri)
-        template += `<div class="user text-center p-2">Name: <strong>${user.name}</strong>, e-mail: <strong>${user.email}</strong></div>`;
+        for (const user of company.users) {
+          if (user.uris.company === company.uri)
+            template += `<div class="user text-center p-2">Name: <strong>${user.name}</strong>, e-mail: <strong>${user.email}</strong></div>`;
+        }
+
+        template += `</div></div>`;
+      }
+      template += "</div>";
+
+      return template;
     }
 
-    template += `</div></div>`;
+    function prepareData(companies, users) {
+      let data = companies.map(company => {
+        company.users = users.filter(user => user.uris.company === company.uri);
+        return company;
+      });
+
+      return data.sort((a, b) => b.users.length - a.users.length);
+    }
+
+    function getData() {
+      return Promise.all([
+        fetch(`${apiUrl}/companies`).then(res => res.json()),
+        fetch(`${apiUrl}/users`).then(res => res.json())
+      ]);
+    }
+
+    function init() {
+      getData
+        .then(([companies, users]) => {
+          const dataToRender = prepareData(companies, users);
+          document.querySelector('.list').innerHTML = createDom(dataToRender);
+        })
+        .catch(err => console.log('Rejected: ', err.message));
+    }
+
+    init();
+
   }
-  template += "</div>";
-
-  return template;
-}
-
-Promise.all([
-  fetch(`${apiUrl}/companies`).then(res => res.json()),
-  fetch(`${apiUrl}/users`).then(res => res.json())
-])
-  .then(([companies, users]) => {
-
-    let dataToRender = companies.map(company => {
-      company.users = users.filter(user => user.uris.company === company.uri);
-
-      return company;
-    });
-
-    dataToRender = dataToRender.sort((a, b) => b.users.length - a.users.length);
-
-    document.querySelector('.list').innerHTML = createDom(dataToRender);
-
-  })
-  .catch(err => console.log('Rejected: ', err.message));
-
+};
