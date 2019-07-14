5f0844278c3d1e448ae0f3f752aca3524a93f21e
laszarm <hcrgames@gmail.com>
2019-07-14 21:43:58 +0200

    clean up

--- app/data.js
+++ app/data.js
@@ -1,23 +1,15 @@
 const apiUrl = "http://localhost:3000";
-// let preparedData = [];
-
-// function pagination(page = 1, quantity = 20) {
-//   let data = preparedData.slice((page-1)*quantity, (page*quantity));
-//
-//   document.querySelector('.list').innerHTML = createDom(data);
-// }
 
 function createDom(data) {
   let template = '<div class="container">';
 
-  for (let i=0; i<data.length-1;i++) {
-    let company = data[i];
+  for (const [index, company] of data.entries()) {
     template += `<div class="d-flex flex-column">
-                  <button type="button" class="p-2 list-group-item list-group-item-action text-center" data-toggle="collapse" href="#collapse${i}" role="button" aria-expanded="false" aria-controls="collapse${i}">
+                  <button type="button" class="p-2 list-group-item list-group-item-action text-center" data-toggle="collapse" href="#collapse${index}" role="button" aria-expanded="false" aria-controls="collapse${index}">
                     ${company.name} <span class="badge badge-primary badge-pill">${company.users.length}</span>
                   </button>`;
 
-    template += `<div class="collapse" id="collapse${i}">`;
+    template += `<div class="collapse" id="collapse${index}">`;
 
     for (const user of company.users) {
       if (user.uris.company === company.uri)
@@ -36,7 +28,6 @@ Promise.all([
   fetch(`${apiUrl}/users`).then(res => res.json())
 ])
   .then(([companies, users]) => {
-    // console.log("companies, users: ", companies, users);
 
     let dataToRender = companies.map(company => {
       company.users = users.filter(user => user.uris.company === company.uri);
@@ -46,11 +37,7 @@ Promise.all([
 
     dataToRender = dataToRender.sort((a, b) => b.users.length - a.users.length);
 
-    // preparedData = dataToRender;
     document.querySelector('.list').innerHTML = createDom(dataToRender);
-    // pagination();
-
-    // console.log("data to render: ", dataToRender);
 
   })
   .catch(err => console.log('Rejected: ', err.message));
