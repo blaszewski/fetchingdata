da8a9788a2f63e62173b5d161e770512147a392e
laszarm <hcrgames@gmail.com>
2019-07-14 22:52:55 +0200

    clean up and typeo fix

--- app/data.js
+++ app/data.js
@@ -1,6 +1,5 @@
 document.onreadystatechange = () => {
-  if (document.readyState === 'complete') {
-
+  if (document.readyState === "complete") {
     const apiUrl = "http://localhost:3000";
 
     function createDom(data) {
@@ -9,14 +8,20 @@ document.onreadystatechange = () => {
       for (const [index, company] of data.entries()) {
         template += `<div class="d-flex flex-column">
                 <button type="button" class="p-2 list-group-item list-group-item-action text-center" data-toggle="collapse" href="#collapse${index}" role="button" aria-expanded="false" aria-controls="collapse${index}">
-                    ${company.name} <span class="badge badge-primary badge-pill">${company.users.length}</span>
+                    ${
+                      company.name
+                    } <span class="badge badge-primary badge-pill">${
+          company.users.length
+        }</span>
                 </button>`;
 
         template += `<div class="collapse" id="collapse${index}">`;
 
         for (const user of company.users) {
           if (user.uris.company === company.uri)
-            template += `<div class="user text-center p-2">Name: <strong>${user.name}</strong>, e-mail: <strong>${user.email}</strong></div>`;
+            template += `<div class="user text-center p-2">Name: <strong>${
+              user.name
+            }</strong>, e-mail: <strong>${user.email}</strong></div>`;
         }
 
         template += `</div></div>`;
@@ -43,15 +48,14 @@ document.onreadystatechange = () => {
     }
 
     function init() {
-      getData
+      getData()
         .then(([companies, users]) => {
           const dataToRender = prepareData(companies, users);
-          document.querySelector('.list').innerHTML = createDom(dataToRender);
+          document.querySelector(".list").innerHTML = createDom(dataToRender);
         })
-        .catch(err => console.log('Rejected: ', err.message));
+        .catch(err => console.log("Rejected: ", err.message));
     }
 
     init();
-
   }
 };
